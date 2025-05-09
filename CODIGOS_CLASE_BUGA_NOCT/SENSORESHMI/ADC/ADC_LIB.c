dd756632b23b1a700b640e982db5c5dc4f9e947f
iSebas <jusecofe@gmail.com>
2025-05-08 19:56:45 -0500

    Se agrega librerias

---
+++ CODIGOS_CLASE_BUGA_NOCT/SENSORESHMI/ADC/ADC_LIB.c
@@ -0,0 +1,6 @@
+/*
+ * ADC_LIB.c
+ *
+ * Created: 08/05/2025 07:47:23 p. m.
+ *  Author: USER
+ */ 
---
+++ CODIGOS_CLASE_BUGA_NOCT/SENSORESHMI/main.c
@@ -0,0 +1,18 @@
+/*
+ * SENSORESHMI.c
+ *
+ * Created: 08/05/2025 07:41:06 p. m.
+ * Author : USER
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
